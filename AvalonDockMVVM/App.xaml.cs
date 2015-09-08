317af615d7a2aef7c690f80e3bef10b0bf8e1a3f
mdk <mdk@lostindetails.com>
2015-09-08 14:36:35 +0200

    initial commit

---
+++ AvalonDockMVVM/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using AvalonDockMVVM.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace AvalonDockMVVM
+{
+  /// <summary>
+  /// Interaction logic for App.xaml
+  /// </summary>
+  public partial class App : Application
+  {
+    private void Application_Startup(object sender, StartupEventArgs e)
+    {
+      /* create the window */
+      var mainWindow = new MainWindow();
+      this.MainWindow = mainWindow;
+      mainWindow.Show();
+
+      /* attach the mainViewModel */
+      var mainViewModel = new MainViewModel();
+      mainWindow.DataContext = mainViewModel;
+    }
+  }
+}
---
+++ AvalonDockMVVM/ViewModel/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvalonDockMVVM.ViewModel
+{
+  public abstract class BaseViewModel: INotifyPropertyChanged
+  {
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged(string propertyName)
+    {
+      this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+  }
+
+}
